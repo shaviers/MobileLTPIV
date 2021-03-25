b71173132a93a5ae10c61bb9d3df2cfe0a982603
shaviers <70414957+shaviers@users.noreply.github.com>
2021-03-24 22:58:47 -0300

    Add files via upload

---
+++ Trabalho I/AULA1/AULA1/AULA1/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AULA1.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AULA1.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Trabalho I/AULA1/AULA1/AULA1/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AULA1.Models;
+using AULA1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AULA1.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
